c8d03c14c685971506fec443c9146faeef4243b3
Vo Trung Chi <votrungchi_108@live.com>
2026-01-04 15:32:18 +0700

    add Neo-tree and update keymap
    Signed-off-by: Vo Trung Chi <votrungchi_108@live.com>

--- init.lua
+++ init.lua
@@ -1,46 +1,69 @@
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 vim.o.number = true
 vim.o.relativenumber = true
-
-vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
-})
-
+vim.o.signcolumn = 'yes'
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.list = true
 vim.o.confirm = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 0
+vim.o.softtabstop = -1
+vim.o.expandtab = true
+vim.o.shiftround = true
+vim.o.smartindent = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.wrap = false
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
-vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
-vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
-vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
-vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
-vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
-vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
-vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
+-- Use system clipboard
+vim.api.nvim_create_autocmd('UIEnter', {
+  callback = function()
+    vim.o.clipboard = 'unnamedplus'
+  end,
+})
+
+-- Trim trailing whitespace on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function()
+    local cur = vim.api.nvim_win_get_cursor(0)
+    vim.cmd [[%s/\s\+$//e]]
+    pcall(vim.api.nvim_win_set_cursor, 0, cur)
+  end,
+})
+
+-- Keymaps
+vim.keymap.set({ 'n' }, '<M-h>', '<C-w>h')
+vim.keymap.set({ 'n' }, '<M-j>', '<C-w>j')
+vim.keymap.set({ 'n' }, '<M-k>', '<C-w>k')
+vim.keymap.set({ 'n' }, '<M-l>', '<C-w>l')
+vim.keymap.set({ 'n' }, '<leader>c', ':CopilotChat<CR>')
+vim.keymap.set({ 'n' }, '<leader>t', ':terminal<CR>')
+vim.keymap.set({ 'n' }, '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set({ 'n' }, '<leader>f', ':Pick files<CR>')
+vim.keymap.set({ 'n' }, '<leader>b', ':Pick buffers<CR>')
+vim.keymap.set({ 'n' }, '<leader>h', ':Gitsigns preview_hunk_inline<CR>')
+vim.keymap.set({ 'n' }, '<leader>s', ':Gitsigns show_commit<CR>')
+vim.keymap.set({ 'n' }, '<leader>r', ':Gitsigns reset_hunk<CR>')
+vim.keymap.set({ 'n' }, '<leader>n', ':Neotree reveal<CR>')
 
 vim.cmd('packadd! nohlsearch')
 vim.cmd.colorscheme("retrobox")
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 0
-vim.opt.softtabstop = -1
-vim.opt.expandtab = true
-vim.opt.shiftround = true
-vim.opt.smartindent = true
-
+-- Plugins
 vim.pack.add({
-  {src = "https://github.com/github/copilot.vim"},
   {src = "https://github.com/nvim-lua/plenary.nvim"},
+  {src = "https://github.com/MunifTanjim/nui.nvim"},
+  {src = "https://github.com/github/copilot.vim"},
   {src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim"},
   {src = "https://github.com/nvim-mini/mini.pick"},
   {src = "https://github.com/lewis6991/gitsigns.nvim"},
+  {src = "https://github.com/nvim-neo-tree/neo-tree.nvim", version = "v3.x"},
 })
 
 require("mini.pick").setup({
@@ -50,6 +73,33 @@ require("mini.pick").setup({
   }
 })
 
-require("CopilotChat").setup()
-require("gitsigns").setup()
+require("CopilotChat").setup({
+    headers = {
+      user = '👷 Chi Vo',
+      assistant = '🤡 Copilot',
+      tool = '🔧 Tool',
+    },
+    model = 'gpt-4.1',
+    temperature = 0.1,
+    window = {
+      layout = 'vertical',
+      width = 0.5,
+    },
+    separator = '━━',
+    auto_fold = false,
+    auto_insert_mode = true,
+})
+
+require("gitsigns").setup({
+  current_line_blame = true,
+})
+
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    },
+  },
+})
 
